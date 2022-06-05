003948cc6823a3da09d1a68e28fc0957c11f70c5
Elbek-ibn-Muzaffar <elbekusmonjonov007@gmail.com>
2022-06-05 10:53:53 +0500

    aded some changes

--- src/main/java/com/juniper/emed/controller/RoleController.java
+++ src/main/java/com/juniper/emed/controller/RoleController.java
@@ -7,9 +7,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/role")
@@ -25,5 +28,11 @@ public class RoleController {
         return
         ResponseEntity.ok(roleRepository.save(modelMapper.map(roleDto,Roles.class)));
 
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Roles>> getAllRoles()
+    {
+        return ResponseEntity.ok(roleRepository.findAll());
     }
 }
